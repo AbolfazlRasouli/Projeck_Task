dfac965017e9efc035d5515e8832b2b02c148d66
abolfazl <abolfazlrasouli95@gmail.com>
2023-11-25 10:30:55 +0330

    change customeuser

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import CustomerUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomerUser
+    list_display = ['username', 'email', 'birth_day', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('birth_day',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('birth_day',)}),
+    )
+
+
+admin.site.register(CustomerUser, CustomUserAdmin)
